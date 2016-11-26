8c032dec7a861c473858c96f35eecf34462f037f
JhumanJ <julien@nahum.net>
2016-11-26 14:46:12 +0000

    add everything missing temporarly

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name','last_name', 'email', 'password','birth_date','sexual_orientation','phone_number'
+        'first_name','last_name', 'email', 'password','birth_date','sexual_orientation','phone_number','status'
     ];
 
     /**
